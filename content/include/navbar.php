adb233f8355d66610eb514ecc9355a417759b4bd
Florian Gaudin <floriangaudin13@gmail.com>
2023-03-16 20:00:48 +0100

    navbar terminée : ajout d'une searchbar dans la navbar

--- content/include/navbar.php
+++ content/include/navbar.php
@@ -121,7 +121,7 @@ session_start();
         <div class="absolute inset-y-0 left-0 flex items-center pl-1 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
         </div>
-        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 p-1" placeholder="Chercher un film" required>
+        <input type="text" id="simple-search" class="bg-primary border border-gray-300 text-bg1 text-impact text-sm placeholder:text-impact placeholder:text-bg1 placeholder:text-bold rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-7 p-1" placeholder="Chercher un film" required>
     </div>
     <button type="submit" class="p-1 ml-2 text-sm font-medium text-third bg-orange-400 rounded-lg border border-blue-700 hover:bg-third hover:text-secondary focus:ring-4 focus:outline-none focus:ring-blue-300">
         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -130,7 +130,7 @@ session_start();
 </form>
 
     <!-- bouton se connecter / s'inscrire version tablette/desktop -->
-    <div class="hidden md:flex flex-col items-center">
+    <div class="hidden md:flex flex-col items-center mr-3">
             <a href="#" class=""><img class="h-20 w-auto" src="./assets/img/login.png" alt="logo metromax"></a>
             <a href="#" class="-mx-3 rounded-lg py-2 px-3 text-base leading-7 text-primary font-impact uppercase">Se connecter / S'inscrire</a>
     </div>
